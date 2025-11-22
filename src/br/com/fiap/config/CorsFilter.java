365dead61c06ba388cc7ced7845b478f68122750
Samara Vilela <samara.vilela.oliveira@gmail.com>
2025-11-22 09:09:24 -0300

    AJUSTE CORS

--- src/br/com/fiap/config/CorsFilter.java
+++ src/br/com/fiap/config/CorsFilter.java
@@ -2,15 +2,33 @@ package br.com.fiap.config;
 
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
 import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
 import java.io.IOException;
 
 /**
  * Filtro CORS para permitir requisições do frontend
+ * Trata requisições OPTIONS (preflight) e adiciona headers CORS
  */
 @Provider
-public class CorsFilter implements ContainerResponseFilter {
+public class CorsFilter implements ContainerRequestFilter, ContainerResponseFilter {
+    
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        // Trata requisições OPTIONS (preflight)
+        if (requestContext.getMethod().equals("OPTIONS")) {
+            Response response = Response.ok()
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Credentials", "true")
+                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD, PATCH")
+                .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, x-requested-with")
+                .header("Access-Control-Max-Age", "3600")
+                .build();
+            requestContext.abortWith(response);
+        }
+    }
     
     @Override
     public void filter(ContainerRequestContext requestContext, 
@@ -21,10 +39,12 @@ public class CorsFilter implements ContainerResponseFilter {
             "Access-Control-Allow-Credentials", "true");
         responseContext.getHeaders().add(
             "Access-Control-Allow-Headers",
-            "origin, content-type, accept, authorization");
+            "origin, content-type, accept, authorization, x-requested-with");
         responseContext.getHeaders().add(
             "Access-Control-Allow-Methods",
-            "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+            "GET, POST, PUT, DELETE, OPTIONS, HEAD, PATCH");
+        responseContext.getHeaders().add(
+            "Access-Control-Max-Age", "3600");
     }
 }
 
